5b263b448d973152080049c785202a2835800f03
Robbie <rob.w.sherman@gmail.com>
2018-03-10 15:49:06 +0000

    FIrst class

---
+++ Order.cs
@@ -0,0 +1,7 @@
+namespace _02_classes
+{
+    public class Order
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}
